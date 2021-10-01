53856122a5aa6bc0b6c57c3c76b428d010ed4dd4
kelvin <seidelkelvin@gmail.com>
2021-10-01 18:29:37 -0300

    new participant

--- services/participant.service.ts
+++ services/participant.service.ts
@@ -122,13 +122,13 @@ async function GetParticipantStartTotal({ req, res }: Context) {
     res.status(200).json(body)
 }
 
-/* ☝️ PUT Update a planner */
+/* ☝️ PUT Update a participant */
 async function UpdateParticipantById(rut: string,{ req, res }: Context) {
     const { db, connection, ObjectId } = await createConnection()
     const Participants = db.collection('participant')
     const newId = new ObjectId(req.params.id)
     const resp = Participants.findOneAndUpdate(
-    { "_id": (newId) },
+    { "_id": (req.params.id) },
     { $set: req.body },
     function (err, item) {
         if (err) throw err
