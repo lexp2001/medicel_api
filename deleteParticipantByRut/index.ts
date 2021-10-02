2b72ad182a322fde84f76cb78d5b4488648d3ecf
kelvin <seidelkelvin@gmail.com>
2021-10-02 16:49:36 -0300

    Atualizei o index do DeleteParticipantByRut

--- deleteParticipantByRut/index.ts
+++ deleteParticipantByRut/index.ts
@@ -2,7 +2,7 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { participantService } from '../services';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-  await participantService.DeleteParticipantByRut(context); // 👈 This calls the participant service
+  await participantService.DeleteParticipantByRut(req.params.rut,context); // 👈 This calls the participant service
 };
 
 export default httpTrigger;
